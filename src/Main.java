e66042819d78d4c19176c9b4e12ec1929e4d1704
Teza222 <evertonrichards0@gmail.com>
2025-03-24 14:38:09 -0500

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    interface Payable{
+        public double getPaymentAmount();
+    }
+
+    class Employee implements Payable {
+        private String firstname;
+        private String lastName;
+        private String socialSecurityNumber;
+
+        @Override
+        public double getPaymentAmount() {
+            return 0;
         }
+    }
+
+    class Invoice implements Payable{
+        private String partNumber;
+        private String partDescription;
+        private int quantity;
+        private double pricePerItem;
+
+        @Override
+        public double getPaymentAmount() {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
     }
 }
